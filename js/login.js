96a66fb00cd333294eaeaf7cc70811fddedb9fe0
shobha9250 <56711324+shobha9250@users.noreply.github.com>
2022-01-28 23:19:55 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -51,6 +51,7 @@ async function onSuccess(googleUser) {
         var jwt = result.data.token;
         // set jwt in cookie with path
         console.log(result.data);
+        console.log(result.user);
         const  user = result.data.user;
         console.log(user);
         const { role } = user;
